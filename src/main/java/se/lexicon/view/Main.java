190a86171e814ef07a4810a58bc84bafc256c5e9
SMASobur <sikdar.se@gmail.com>
2026-03-27 17:25:55 +0100

    update main

--- src/main/java/se/lexicon/view/Main.java
+++ src/main/java/se/lexicon/view/Main.java
@@ -17,7 +17,7 @@ public class Main {
     private static IVendingMachine machine;
 
 
-    static void main() {
+    public static void main(String[] args) {
         // Setup initial products
         setupVendingMachine();
 
